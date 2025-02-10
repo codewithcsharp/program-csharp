539287321071269c6414e2b2491294e60cc27777
Abhitosh Kumar <kumarabhitosh678@gmail.com>
2025-02-10 11:50:21 +0530

    solid principle dip changes

---
+++ Diffrent.Practice.Solution.Pattern/SOLID/DIP/SolutionStatement/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using Diffrent.Practice.Solution.Pattern.SOLID.DIP.SolutionStatement.Interfaces;
+
+namespace Diffrent.Practice.Solution.Pattern.SOLID.DIP.SolutionStatement
+{
+    public class EmailService : INotificationService
+    {
+        public void SendNotification()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
